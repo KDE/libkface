fe0d5dcd86d54c1ae23d822e38097d85269c876a
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-16 10:09:01 +0000

    usual files for a stand-alone library. not yet compilable
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1138589

---
+++ libkface.pc.cmake
@@ -0,0 +1,12 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}
+
+Name: libkexiv2
+Description: A C++ library to manipulate EXIF/IPTC/XMP metadata using Exiv2 library. This library is used by digiKam and kipi-plugins.
+URL: http://www.kipi-plugins.org
+Requires:
+Version: ${KEXIV2_LIB_VERSION_STRING}
+Libs: -L${LIB_INSTALL_DIR} -lkexiv2
+Cflags: -I${INCLUDE_INSTALL_DIR}
