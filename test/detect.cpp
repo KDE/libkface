6f5582062eebdf84ab86f5b2af5b8c01501970f2
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-17 13:02:03 +0000

    better compilation
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1139138

--- test/detect.cpp
+++ test/detect.cpp
@@ -6,8 +6,8 @@
 #include <QDebug>
 #include <QGraphicsPixmapItem>
 
-#include <libkface/database.h>
-#include <libkface/kface.h>
+#include "libkface/database.h"
+#include "libkface/kface.h"
 
 using namespace KFace;
 
@@ -18,25 +18,24 @@ int main(int argc, char **argv) {
         qDebug()<<"Bad Args!!!\nUsage: "<<argv[0] << " <image1> <image2> ...";
         return 0;
     }
-    
+
     QApplication app(argc, argv);
 
     // Set up a View and Scene
     QGraphicsView *myView = new QGraphicsView();
     QGraphicsScene *myScene = new QGraphicsScene();
     myView->setScene(myScene);
-    
+
     // Load an image from a file and add it to scene
     QPixmap *p = new QPixmap(argv[1]);
     QGraphicsPixmapItem* pItem = new QGraphicsPixmapItem(*p);
     myScene->addItem(pItem);
-    
+
     myView->show();
-    
+
     // Make a new instance of Database and then detect faces from the image
     KFace::Database *d = new KFace::Database(KFace::Database::InitDetection, "");
     QList<Face> result = d->detectFaces(p->toImage());
-    
 
     return app.exec();
 }
