c9b148f37a681bced77855e1700add8d6501dedc
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-17 13:16:34 +0000

    always publish code with licence. Use right header for that.
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1139145

--- test/detect.cpp
+++ test/detect.cpp
@@ -1,17 +1,43 @@
-#include <QApplication>
+/** ===========================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * @date  : 2010-06-16
+ * @note  : The Database class wraps the libface database
+ *
+ * @author: Copyright (C) 2010 by Aditya Bhatt <adityabhatt1991 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
+// Qt includes
+
+#include <QApplication>
 #include <QImage>
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QDebug>
 #include <QGraphicsPixmapItem>
 
+// Local includes
+
 #include "libkface/database.h"
 #include "libkface/kface.h"
 
 using namespace KFace;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc<2)
     {
@@ -39,5 +65,3 @@ int main(int argc, char **argv)
 
     return app.exec();
 }
-
-
