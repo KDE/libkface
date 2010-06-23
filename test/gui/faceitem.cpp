b4f44ac27374618bf338949a616659923d226957
Aditya Jitesh Bhatt <adityabhatt1991@gmail.com>
2010-06-23 09:34:36 +0000

    Added lots of comments to the tagging widget code. And adjusted name input's coordinates.
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1141699

--- test/gui/faceitem.cpp
+++ test/gui/faceitem.cpp
@@ -41,35 +41,46 @@ FaceItem::FaceItem(QGraphicsItem* parent, QGraphicsScene* scene, const QRect& re
 {
     faceRect = new QGraphicsRectItem( 0, scene);
 
+    // Scale all coordinates to fit the initial size of the scene
     x1 = rect.topLeft().x()*scale;
     y1 = rect.topLeft().y()*scale;
     x2 = rect.bottomRight().x()*scale;
     y2 = rect.bottomRight().y()*scale;
 
+    // A QRect containing coordinates for the face rectangle
     QRect scaledRect;
     scaledRect.setTopLeft(QPoint(x1, y1));
     scaledRect.setBottomRight(QPoint(x2, y2));
 
+    // Draw the Face Rectangle as a QGraphicsRectItem using the dimensions of the scaledRect
     faceRect->setRect(scaledRect);
     QPen pen(QColor(QString("red")));
     pen.setWidth(3);
     faceRect->setPen(pen);
-    faceRect->setOpacity(0.5);
+    faceRect->setOpacity(1);
 
-    int x = x1 + 10;
+    // Top-left Coordinates for the name
+    int x = x1 - 20;
     int y = y2 + 10;
 
+    // Make a new QGraphicsTextItem for writing the name text, and a new QGraphicsRectItem to draw a good-looking, semi-transparent bounding box.
     nameRect = new QGraphicsRectItem( 0, scene);
     faceName = new QGraphicsTextItem (name, 0, scene);
 
+    // Make the bounding box for the name update itself to cover all the text whenever contents are changed
     QTextDocument* doc;
     doc = faceName->document();
     connect(doc, SIGNAL(contentsChanged()), 
             this, SLOT(update()));
 
+    // Set coordinates of the name
     faceName->setPos(x,y);
+    faceName->setTextWidth((x2-x1)>140?(x2-x1):140);
 
+    // Get coordinates of the name relative the the scene
     QRectF r = faceName->mapRectToScene(faceName->boundingRect());
+    
+    // Draw the bounding name rectangle with the scene coordinates
     nameRect->setRect(r);
     QPen p(QColor(QString("black")));
     p.setWidth(3);
@@ -77,14 +88,13 @@ FaceItem::FaceItem(QGraphicsItem* parent, QGraphicsScene* scene, const QRect& re
     nameRect->setBrush(QBrush(QColor(QString("black"))));
     nameRect->setOpacity(0.8);
 
+    // Draw the name input item
     faceName->setDefaultTextColor(QColor(QString("white")));
     faceName->setFont(QFont("Helvetica", 10));
     faceName->setTextInteractionFlags(Qt::TextEditorInteraction);
-    faceName->setTextWidth((x2-x1)>300?(x2-x1):140);
-    r = faceName->mapRectToScene(faceName->boundingRect());
-
-    //faceName->setGraphicsEffect();
+    // If the width of the name rect is greater than 140, then let the string extend upto the width. Else keep the length to a max of 140 px.
     faceName->setOpacity(1);
+    
 }
 
 QRectF FaceItem::boundingRect() const
