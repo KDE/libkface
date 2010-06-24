5bd85ecd590787a23695f5e2aed4a4eb67133bf3
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-24 10:42:48 +0000

    wrong declaration. Why GCC do not cry there
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1142194

--- libkface/database.h
+++ libkface/database.h
@@ -62,27 +62,27 @@ public:
     };
     Q_DECLARE_FLAGS(InitFlags, InitFlag)
 
-    /** 
+    /**
      * Constructor for Database 
-     * @param InitFlags Flags that specify type of usage for the Database instance. 
+     * @param flags Flags that specify type of usage for the Database instance. 
      * For Detection : InitDetection 
      * For Recognition : InitRecognition
      * For All : InitAll
      * @param configurationPath The path where the Database configuration file will be stored.
      */
-    Database(InitFlags = InitAll, const QString& configurationPath = QString());
-    
+    Database(InitFlags flags = InitAll, const QString& configurationPath = QString());
+
     /**
      * Copy constructor for database
      * @param other A reference to another database
      */
     Database(const Database& other);
-    
+
     /**
      * Descructor
      */
     ~Database();
-    
+
     /**
      * Assignment Operator.
      * @param other The database whose contents are to be assigned.
@@ -117,13 +117,13 @@ public:
      * @return A QList of "closeness" of recognized faces, in the same order as the argument
      */
     QList<double> recognizeFaces(QList<Face>& faces);
-    
+
     /**
      * Returns the directory path of the config file
      *
      */
     QString configPath();
-    
+
     /**
      * Set the accuracy of Face Detection. This is measured on a 5-point scale, from 1 to 5.
      * 1 means least accuracy, but very high speed. 5 means highest accuracy but very low speed.
@@ -131,7 +131,7 @@ public:
      * @param value The degree of accuracy (1 to 5)
      */
     void setDetectionAccuracy(int value);
-    
+
     /**
      * Get the currently set accuracy of Face Detection on a 5-point scale.
      * 1 means least accuracy, but very high speed. 5 means highest accuracy but very low speed.
