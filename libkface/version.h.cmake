c4599f16dba1bc7673d729527cc2e47e361340aa
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-24 11:01:56 +0000

    polish
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1142198

--- libkface/version.h.cmake
+++ libkface/version.h.cmake
@@ -3,11 +3,11 @@
  * This file is a part of digiKam project
  * <a href="http://www.digikam.org">http://www.digikam.org</a>
  *
- * @date  : 2010-06-16
- * @brief : LibFace library interface for KDE
+ * @date   2010-06-16
+ * @brief  LibFace library interface for KDE
  *
- * @author: Copyright (C) 2010 by Gilles Caulier
- *          <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ * @author Copyright (C) 2010 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
