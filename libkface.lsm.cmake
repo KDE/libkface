cc955d1c0b9117d6ea69e42845c7781d529757f9
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-29 12:27:03 +0000

    update
    svn path=/branches/extragear/graphics/digikam/libkface/; revision=1144140

--- libkface.lsm.cmake
+++ libkface.lsm.cmake
@@ -3,7 +3,7 @@ Title:          libkface
 Version:        ${KFACE_LIB_VERSION_STRING}
 Entered-date:   2010-16-06
 Description:    A C++ wrapper around LibFace library to perform face recognition over pictures.
-Keywords:       kde image jpeg raw tiff png exif iptc xmp digikam kipi-plugins
+Keywords:       kde image face detection recognition digikam kipi-plugins
 Author:         caulier dot gilles at gmail dot com (Gilles Caulier)
                 marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
                 adityabhatt1991 at gmail dot com (Aditya Bhatt)
