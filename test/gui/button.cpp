bb6858fd6a928e812ea9af2efba0a32bcfd946b3
Aditya Jitesh Bhatt <adityabhatt1991@gmail.com>
2010-06-30 14:08:16 +0000

    Copyright notice for Frederico Duarte.
    svn path=/branches/extragear/graphics/libkface/; revision=1144671

--- test/gui/button.cpp
+++ test/gui/button.cpp
@@ -4,10 +4,9 @@
  * <a href="http://www.digikam.org">http://www.digikam.org</a>
  *
  * @date   2010-06-29
- * @brief  GUI test program for libkface
+ * @brief  Pressable Button class using QGraphicsItem
  *
- * @author Copyright (C) 2010 by Aditya Bhatt
- *         <a href="mailto:adityabhatt1991 at gmail dot com">adityabhatt1991 at gmail dot com</a>
+ * @author Copyright (C) 2010 by Frederico Duarte
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
