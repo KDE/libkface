660798cf7429769b0fe12dfe5bedc52e5102b7f0
Mahesh Hegde <maheshmhegade@gmail.com>
2013-02-25 11:16:09 +0530

    opentld added

---
+++ OpenTLD/src/opentld/main/Main.cpp
@@ -0,0 +1,44 @@
+#include "Main.h"
+
+#include "Config.h"
+#include "TLDUtil.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+using namespace tld;
+using namespace cv;
+
+unitFaceModel * Main::generatefacemodel(IplImage* img)
+{
+    Mat grey(img->height, img->width, CV_8UC1);
+
+    cvtColor(cv::Mat(img), grey,CV_BGR2GRAY);// CV_BGR2GRAY);
+    //imshow("ty",im);
+    tld->detectorCascade->imgWidth = grey.cols;
+    tld->detectorCascade->imgHeight = grey.rows;
+    tld->detectorCascade->imgWidthStep = grey.step;
+
+    initialBB = new int[4];
+    initialBB[0] = 5;
+    initialBB[1] = 5;
+    initialBB[2] = img->width-5;
+    initialBB[3] = img->height-5;
+    Rect bb = tldArrayToRect(initialBB);
+    tld->selectObject(grey, &bb);
+    cvtColor(cv::Mat(img), grey, CV_BGR2GRAY);
+    return tld->putObjModel();
+}
+float Main::getrecognitionconfidence(IplImage* img, unitFaceModel *comparemodel)
+{
+    Mat grey(img->height, img->width, CV_8UC1);
+
+    cvtColor(cv::Mat(img), grey,CV_BGR2GRAY);// CV_BGR2GRAY);
+    //imshow("ty",im);
+    tld->detectorCascade->imgWidth = grey.cols;
+    tld->detectorCascade->imgHeight = grey.rows;
+    tld->detectorCascade->imgWidthStep = grey.step;
+    tld->getObjModel(comparemodel);//modelPath);
+    cvtColor(cv::Mat(img), grey, CV_BGR2GRAY);
+    tld->processImage(img);
+    return tld->currConf;
+}
