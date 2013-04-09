7d447a2afe43b337f5a7a2074935408d9760c221
Gilles Caulier <caulier.gilles@gmail.com>
2013-04-09 14:19:16 +0200

    fix GCC warnings

--- libkface/image_p.h
+++ libkface/image_p.h
@@ -11,7 +11,7 @@
  *         <a href="mailto:marcel dot wiesweg at gmx dot de">marcel dot wiesweg at gmx dot de</a>
  * @author Copyright (C) 2010 by Aditya Bhatt
  *         <a href="mailto:adityabhatt1991 at gmail dot com">adityabhatt1991 at gmail dot com</a>
- * @author Copyright (C) 2010-2012 by Gilles Caulier
+ * @author Copyright (C) 2010-2013 by Gilles Caulier
  *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
@@ -43,7 +43,8 @@ class Image::ImagePriv : public QSharedData
 public:
 
     ImagePriv()
-        : image(0),originalImage(0)
+        : originalImage(0),
+          image(0)
     {
     }
 
@@ -57,7 +58,8 @@ public:
     {
         if (image)
             cvReleaseImage(&image);
-            cvReleaseImage(&originalImage);
+
+        cvReleaseImage(&originalImage);
     }
 
     IplImage* originalImage;
