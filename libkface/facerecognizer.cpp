c3dc35d200418ed482fb62a6d942840805f0a172
Mahesh Hegde <maheshmhegade@gmail.com>
2013-04-10 01:42:11 +0530

    add facerecognizer

---
+++ libkface/facerecognizer.cpp
@@ -0,0 +1,156 @@
+/** ===========================================================
+ * @file
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date    2010-03-03
+ * @brief   openTLD interface.
+ *
+ * @author Copyright (C) 2012-2013 by Mahesh Hegde
+ *         <a href="mailto:maheshmhegade at gmail dot com">maheshmhegade at gmail dot com</a>
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+//KDE includes
+#include <KDebug>
+
+//Qt includes
+#include <QList>
+
+//local includes
+#include "facerecognizer.h"
+#include "libopencv.h"
+#include "tlddatabase.h"
+#include "tldrecognition.h"
+
+namespace KFaceIface
+{
+
+class FaceRecognizer::FaceRecognizerPriv
+{
+public:
+
+    FaceRecognizerPriv()
+    {
+        db = 0;
+        threshold  = 0.3;
+    }
+
+    ~FaceRecognizerPriv()
+    {
+        delete db;
+    }
+
+public:
+
+    Tlddatabase* database()
+    {
+        if (!db)
+        {
+            db = new Tlddatabase();
+        }
+        return db;
+    }
+
+    Tldrecognition* recognition()
+    {
+        return (new Tldrecognition);
+    }
+
+public:
+
+    float threshold;
+
+private:
+
+    Tldrecognition*  tldRecognitionCore;
+    Tlddatabase* db;
+};
+
+FaceRecognizer::FaceRecognizer()
+    : d(new FaceRecognizerPriv())
+{
+}
+
+FaceRecognizer::~FaceRecognizer()
+{
+}
+
+QList<float> FaceRecognizer::recognizeFaces(QList<Face>& faces)
+{
+    QList<float> recognitionRate;
+    foreach(Face face, faces)
+    {
+        vector<float> recognitionconfidence;
+
+        IplImage *img1 = face.image().toIplImage();
+        IplImage* const inputfaceimage = cvCreateImage(cvSize(47,47),img1->depth,img1->nChannels);
+        cvResize(img1, inputfaceimage);
+
+        int count                      = -1;
+
+        for (int i = 1; i <= d->database()->queryNumfacesinDatabase();i++ )
+        {
+            unitFaceModel* const comparemodel = d->database()->getFaceModel(i);
+            recognitionconfidence.push_back(d->recognition()->getRecognitionConfidence(inputfaceimage,comparemodel));
+            count++;
+        }
+
+        float maxConfidence = recognitionconfidence[0];
+
+        if(count != -1)
+        {
+            int maxConfIndex    = 0;
+
+            for(int tmpInt = 0; tmpInt <= count ; tmpInt++ )
+            {
+                if(recognitionconfidence[tmpInt] > maxConfidence)
+                {
+                    maxConfIndex  = tmpInt;
+                    maxConfidence = recognitionconfidence[tmpInt];
+                }
+            }
+            if(maxConfidence > d->threshold )
+            {
+                face.setName(d->database()->querybyFaceid(maxConfIndex+1));
+                kDebug() << "preson  " << qPrintable(d->database()->querybyFaceid(maxConfIndex+1));
+            }
+        }
+        recognitionRate.append(maxConfidence);
+    }
+    return recognitionRate;
+}
+
+void FaceRecognizer::storeFaces(QList<Face> &faces)
+{
+    foreach(Face face, faces)
+    {
+        IplImage  *img1 = face.image().toIplImage();// tlddatabase->QImage2IplImage(face.image().toQImage());
+
+        IplImage* const inputfaceimage                    = cvCreateImage(cvSize(47,47),img1->depth,img1->nChannels);
+        cvResize(img1,inputfaceimage);
+
+        KFaceIface::unitFaceModel* const facemodeltostore = d->recognition()->getModeltoStore(inputfaceimage);
+        facemodeltostore->Name                            = face.name();
+        facemodeltostore->faceid                          = face.id();
+
+        d->database()->insertFaceModel(facemodeltostore);             //store facemodel in tlddatabase
+    }
+}
+
+} // namespace KFaceIface
+
---
+++ libkface/facerecognizer.h
@@ -0,0 +1,58 @@
+/** ===========================================================
+ * @file
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date    2010-03-03
+ * @brief   openTLD interface.
+ *
+ * @author Copyright (C) 2012-2013 by Mahesh Hegde
+ *         <a href="mailto:maheshmhegade at gmail dot com">maheshmhegade at gmail dot com</a>
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef KFACE_FACERECOGNIZER_H
+#define KFACE_FACERECOGNIZER_H
+
+#include "face.h"
+#include "image.h"
+
+namespace KFaceIface
+{
+
+class FaceRecognizer
+{
+
+public:
+
+    FaceRecognizer();
+    ~FaceRecognizer();
+
+    QList<float> recognizeFaces(QList<Face> &faces);
+    void storeFaces(QList<Face> &faces);
+
+    //void   setThreshold(float value);
+
+private:
+
+    class FaceRecognizerPriv;
+    FaceRecognizerPriv* const d;
+};
+
+} // namespace KFaceIface
+
+#endif // KFACE_FACERECOGNIZER_H
